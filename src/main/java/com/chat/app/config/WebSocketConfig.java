a52f9c1e580b680141afdf55ab43ed423d680e2b
thesaddamsyed <thesaddamsyed@gmail.com>
2025-09-03 13:00:53 +0530

    corrected the cors error

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -33,7 +33,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        // Use allowedOriginPatterns with wildcard to avoid credentials + "*" error
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
