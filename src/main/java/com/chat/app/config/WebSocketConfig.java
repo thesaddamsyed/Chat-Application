b4639460bc91a75c7809644e66c040dca444dbc4
thesaddamsyed <thesaddamsyed@gmail.com>
2025-09-03 12:07:10 +0530

    preparing for deployment

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -8,34 +8,32 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer    {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // Configure the message broker to use a simple in-memory broker
         // and set the prefix for application destinations
         // This allows messages to be sent to specific topics
 
-        registry.enableSimpleBroker("/topic"); 
+        registry.enableSimpleBroker("/topic");
         // This enables a simple in-memory message broker
         // that will route messages to subscribers of the specified topic
-        // For example, messages sent to "/topic/messages" will be broadcast to all subscribers of that topic
+        // For example, messages sent to "/topic/messages" will be broadcast to all
+        // subscribers of that topic
         // Set the prefix for messages that are sent to the server
         // and then routed to specific controllers or services
         // This prefix is used for messages that are sent to the server
         // and then routed to specific controllers or services
-        // For example, messages sent to "/app/sendMessage" will be handled by the ChatController
+        // For example, messages sent to "/app/sendMessage" will be handled by the
+        // ChatController
         registry.setApplicationDestinationPrefixes("/app");
 
     }
 
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:8080").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
-    
 
-    
 }
